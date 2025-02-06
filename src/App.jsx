89423e56cb85b87a876911446a5145889a2438fc
Airam Plasencia <airaplas@gmail.com>
2025-02-06 11:41:28 +0000

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
@@ -5,22 +6,22 @@ import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
 
-function App() {
+ 
+export default function App() {
 
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
 
       <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+        </Routes>
       </div>
 
     </div>
   );
 }
 
-export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "./../assets/logo-ironhack-blue.png";
+import { NavLink, Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -20,6 +21,7 @@ function Navbar() {
 
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
+          <Link to="/profile">
           <button className="flex items-center text-l py-1">
             <img
               src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
@@ -27,7 +29,7 @@ function Navbar() {
               className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
             />
           </button>
-
+          </Link>
         </div>
       </div>
     </nav>
